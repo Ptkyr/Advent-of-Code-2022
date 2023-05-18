d7ad12b1ca1408fd415febfb5713adab66d53771
Ptkyr <eric@GKTL1648.cds.int>
2023-05-18 09:54:02 -0400

    another attempt

--- haskell/07/sol.hs
+++ haskell/07/sol.hs
@@ -1,41 +1,36 @@
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    print . words $ input
+    let tokens = drop 3 . words $ input
+    let filesystem = (buildFile (File "/" 0 0 []) tokens)
+    --print . partOne $ filesystem
+    print filesystem
 
 data File = File
     { name :: String
     , size :: Int
+    , scnt :: Int
     , subs :: [File]
-    , prnt :: Maybe File
     } deriving (Show)
 
-buildFile :: File -> [String] -> File
-buildFile f ("$" : "cd" : ".." : s) = case prnt f of
-    Just p  -> buildFile (p { subs = replaceFile (subs p) f }) s
-    Nothing -> f
-buildFile f ("$" : "cd" : d : s)    = buildFile (findFile (subs f) d) s
+-- Need to return modified input stream
+buildFile :: File -> [String] -> (File, [String])
+buildFile f ("$" : "cd" : ".." : s) = f
+buildFile f ("$" : "cd" : d : s)    = f { scnt = scnt f + 1, subs = (buildFile (File d 0 0 []) s) : (subs f)}
 buildFile f ("$" : "ls" : s)        = buildFile f s
-buildFile f ("dir" : d : s)         = buildFile (addFile f (File d 0 [] (Just f))) s
-buildFile f (x : d : s)             = buildFile (addFile f (File d (read x) [] (Just f))) s
+buildFile f ("dir" : d : s)         = buildFile f s
+buildFile f (x : d : s)             = buildFile (f { scnt = scnt f + 1, subs = (File d (read x) 0 []) : (subs f)}) s
 buildFile f _                       = f
-
-addFile :: File -> File -> File
-addFile p f = p { subs = f : (subs p) }
-
-replaceFile :: [File] -> File -> [File]
-replaceFile [] _ = []
-replaceFile (s : ss) f
-    | name s == name f = f : ss
-    | otherwise        = s : replaceFile ss f
-
-findFile :: [File] -> String -> File
-findFile (f : []) _ = f
-findFile (f : fs) s
-    | name f == s = f
-    | otherwise   = findFile fs s
 
 setFileSize :: File -> File
 setFileSize f = case subs f of
     []        -> f
     otherwise -> f { size = sum . map size . map setFileSize $ (subs f) }
+
+partOne :: File -> Int
+partOne f = isSmall f + (sum . map partOne $ (subs f))
+    where
+    isSmall :: File -> Int
+    isSmall f
+        | size f <= 2000000 = 1
+        | otherwise        = 0
