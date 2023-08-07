f52958a0577e4d584d55f4189227cff2ce87d39f
Ptkyr <ptkyr@ptkyr.localdomain>
2023-08-07 08:21:22 -0400

    abstraction machine

--- haskell/08/sol.hs
+++ haskell/08/sol.hs
@@ -10,102 +10,65 @@ main = do
     print . partOne $ forest
     print . partTwo $ forest
 
-type Arr2DInt = Array (Int, Int) Int
 type Arr2D a = Array (Int, Int) a
 
 zipWithArr :: (a -> a -> b) -> Arr2D a -> Arr2D a -> Arr2D b
 zipWithArr f a1 a2 = array bnds 
-                   $ zip rngs
-                   $ fmap lf rngs
+                   $ zip rang
+                   $ fmap lf rang
     where
         bnds = bounds a1
-        rngs = range bnds
+        rang = range bnds
         lf = liftA2 f (a1 !) (a2 !)
 
-parseToArr :: [String] -> Arr2DInt
+parseToArr :: [String] -> Arr2D Int
 parseToArr s = array bnds 
-             $ zip rngs
+             $ zip rang
              $ concat . map (map digitToInt) $ s
     where
         bnds = ((1, 1), (x, y))
-        rngs = range bnds
+        rang = range bnds
         x = length s
         y = length . head $ s
 
-partOne :: Arr2DInt -> Int
+partOne :: Arr2D Int -> Int
 partOne a = sum . foldr (zipWithArr (ior)) u $ [l, d, r]
     where
         ior :: Int -> Int -> Int
         ior x y = clamp (0, 1) $ x + y
-        u = visU a
-        l = visL a
-        d = visD a
-        r = visR a
+        u = visDir (+ (-1)) id a
+        l = visDir id (+ (-1)) a
+        d = visDir (+ 1) id    a
+        r = visDir id (+ 1)    a
 
-partTwo :: Arr2DInt -> Int
+partTwo :: Arr2D Int -> Int
 partTwo a = maximum . foldr (zipWithArr (*)) u $ [l, d, r]
     where
-        u = viewU a
-        l = viewL a
-        d = viewD a
-        r = viewR a
+        u = viewDist (+ (-1)) id a
+        l = viewDist id (+ (-1)) a
+        d = viewDist (+ 1) id    a
+        r = viewDist id (+ 1)    a
 
-visDir :: (Int -> Int) -> (Int -> Int) 
-         -> Arr2DInt -> Arr2DInt
-visDir fx fy forest 
+valueAt :: Int -> Int -> (Int -> Int -> Int)
+         -> (Int -> Int) -> (Int -> Int) -> Arr2D Int -> Arr2D Int
+valueAt edge halt recur fx fy forest 
     = array bnds
-    $ [((i, j), visComp (forest!(i, j)) (fx i) (fy j))
+    $ [((i, j), wave (forest!(i, j)) (fx i) (fy j))
     | i <- [x..x']
     , j <- [y..y']]
     where
         bnds = bounds forest
         ((x, y), (x', y')) = bnds
-        -- visComp; visible from (u, v) to the edge, step with fx/fy
-        visComp :: Int -> Int -> Int -> Int
-        visComp t u v
-            | not . inRange bnds $ (u, v) = 1
-            | t <= forest!(u, v)          = 0
-            | otherwise                   = 1 * visComp t nx ny
+        -- wave; go from (u, v) to the edge, stepping with fx/fy
+        wave :: Int -> Int -> Int -> Int
+        wave t u v
+            | not . inRange bnds $ (u, v) = edge
+            | t <= forest!(u, v)          = halt
+            | otherwise                   = recur 1 $ wave t nx ny
             where (nx, ny) = (fx u, fy v)
 
-viewDist :: (Int -> Int) -> (Int -> Int) 
-         -> Arr2DInt -> Arr2DInt
-viewDist fx fy forest 
-    = array bnds
-    $ [((i, j), vCount (forest!(i, j)) (fx i) (fy j))
-    | i <- [x..x']
-    , j <- [y..y']]
-    where
-        bnds = bounds forest
-        ((x, y), (x', y')) = bnds
-        -- vCount; # visible from (u, v) to the edge, step with fx/fy
-        vCount :: Int -> Int -> Int -> Int
-        vCount t u v
-            | not . inRange bnds $ (u, v) = 0
-            | t <= forest!(u, v)          = 1
-            | otherwise                   = 1 + vCount t nx ny
-            where (nx, ny) = (fx u, fy v)
+visDir :: (Int -> Int) -> (Int -> Int) -> Arr2D Int -> Arr2D Int
+visDir   = valueAt 1 0 (*) 
 
-visU :: Arr2DInt -> Arr2DInt
-visU = visDir (+ (-1)) id
-
-visL :: Arr2DInt -> Arr2DInt
-visL = visDir id (+ (-1))
-
-visD :: Arr2DInt -> Arr2DInt
-visD = visDir (+ 1) id
-
-visR :: Arr2DInt -> Arr2DInt
-visR = visDir id (+ 1)
-
-viewU :: Arr2DInt -> Arr2DInt
-viewU = viewDist (+ (-1)) id
-
-viewL :: Arr2DInt -> Arr2DInt
-viewL = viewDist id (+ (-1))
-
-viewD :: Arr2DInt -> Arr2DInt
-viewD = viewDist (+ 1) id
-
-viewR :: Arr2DInt -> Arr2DInt
-viewR = viewDist id (+ 1)
+viewDist :: (Int -> Int) -> (Int -> Int) -> Arr2D Int -> Arr2D Int
+viewDist = valueAt 0 1 (+) 
