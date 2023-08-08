b1b6cad50e0b378642bf766dd744cdb8cbcba1db
Ptkyr <ptkyr@ptkyr.localdomain>
2023-08-07 20:29:24 -0400

    refactors

--- haskell/07/sol.hs
+++ haskell/07/sol.hs
@@ -4,25 +4,26 @@ import Text.Read (readMaybe)
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    let ignoreRoot = drop 3 . words $ input
-    let system = mkFS rootFile $ ignoreRoot
+    let system = mkFS rootDir . drop 3 . words $ input
     print . partOne $ system
     print . partTwo $ system
 
-data File = File
+-- Files won't actually exist, I only need their sizes
+-- Names also don't really matter, but having / is nice
+data Dir = Dir
     { sz :: Int
     , nm :: String
-    , ch :: [File]
-    , pn :: Maybe File
+    , ch :: [Dir]
+    , pn :: Maybe Dir
     }
 
-rootFile :: File
-rootFile = File 0 "/" [] Nothing
+rootDir :: Dir
+rootDir = Dir 0 "/" [] Nothing
 
-mkFS :: File -> [String] -> File
+mkFS :: Dir -> [String] -> Dir
 mkFS f cmd = case cmd of
     "$" : "cd" : ".." : xs -> unroll p xs
-    "$" : "cd" : _ : xs    -> mkFS (File 0 "" [] $ Just f) xs
+    "$" : "cd" : _ : xs    -> mkFS (Dir 0 "" [] $ Just f) xs
     x : _ : xs -> case readMaybe x of
         Just num           -> mkFS f {sz = num + sz f} xs
         Nothing            -> mkFS f xs
@@ -31,24 +32,24 @@ mkFS f cmd = case cmd of
     where 
         p = pn f
         -- unroll; after making a dir, insert into parent
-        unroll :: Maybe File -> [String] -> File
+        unroll :: Maybe Dir -> [String] -> Dir
         unroll (Just d) cs = mkFS d {ch = f : ch d, sz = sz d + sz f} cs
-        unroll Nothing  _  = rootFile -- Error, unreachable
+        unroll Nothing  _  = error "Unreachable"
 
-partOne :: File -> Int
+partOne :: Dir -> Int
 partOne f = isSmall f + (sum . map partOne . ch $ f)
     where 
-        isSmall :: File -> Int
+        isSmall :: Dir -> Int
         isSmall t = if x <= 100000 then x else 0
             where x = sz t
         
-p2min :: File -> Int
+p2min :: Dir -> Int
 p2min rt = 30000000 - 70000000 + sz rt
 
-partTwo :: File -> Int
+partTwo :: Dir -> Int
 partTwo rt = p2FixT (gtmin . p2min $ rt) rt
     where
-        p2FixT :: (Int -> Int -> Int) -> File -> Int
+        p2FixT :: (Int -> Int -> Int) -> Dir -> Int
         -- Order moot, but foldl' is faster
         p2FixT m f = foldl' m (sz f) $ map (p2FixT m) . ch $ f
         -- gtmin; take min of x y above t, 0 iff both below
