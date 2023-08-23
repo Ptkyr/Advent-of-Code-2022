2c49ff91be560df6f7069542ee1cc0eb78effbcc
Ptkyr <ptkyr@ptkyr.localdomain>
2023-08-23 19:35:51 -0400

    spacing

--- haskell/10/10.hs
+++ haskell/10/10.hs
@@ -24,26 +24,25 @@ aocParse = many parseInst <* eof
 execute :: CPU -> [Inst] -> [CPU]
 execute _ []                 = []
 execute cpu@(x, c) ((act, cyc) : is)
-    | cyc == 1    = cpu : execute (act x, c') is
-    | otherwise   = cpu : execute (x, c') ((act, cyc - 1) : is)
-    where c' = c + 1
+    | cyc == 1    = cpu : execute (act x, c + 1) is
+    | otherwise   = cpu : execute (x, c + 1) ((act, cyc - 1) : is)
 
 partOne :: [Inst] -> Int
 partOne = sum . map report . execute (1, 1)
     where 
-        report :: CPU -> Int
-        report (x, c)
-            | c `rem` 40 == 20 = x * c
-            | otherwise        = 0
+    report :: CPU -> Int
+    report (x, c)
+        | c `rem` 40 == 20 = x * c
+        | otherwise        = 0
 
 partTwo :: [Inst] -> String
 partTwo = concat . map drawer . execute (1, 1)
     where
-        drawer :: CPU -> String
-        drawer (x, c) = disply ++ endLn
-            where 
-                crtPos = (c - 1) `rem` 40
-                endLn  = if crtPos == 39 then "\n"
-                                         else ""
-                disply = if abs (x - crtPos) < 2 then "#"
-                                                 else "."
+    drawer :: CPU -> String
+    drawer (x, c) = disply ++ endLn
+        where 
+        crtPos = (c - 1) `rem` 40
+        endLn  = if crtPos == 39 then "\n"
+                                 else ""
+        disply = if abs (x - crtPos) < 2 then "#"
+                                         else "."
