fa9ea75121a9392cff3b97ec64b4a5bc715576a8
Ptkyr <eric@GKTL1648.cds.int>
2023-08-24 09:56:59 -0400

    day 3 parsing

--- haskell/03/03.hs
+++ haskell/03/03.hs
@@ -1,34 +1,52 @@
+import Utils
+
 main :: IO ()
 main = do
-    input <- readFile "03/input.txt"
-    let sacks = words input
-    print $ partOne sacks
-    print $ partTwo sacks
+    parsed <- parseInput aocParse "03/input.txt"
+    case parsed of
+        Left pError -> putStr $ errorBundlePretty pError
+        Right input -> do
+            print $ partOne input
+            print $ partTwo input
 
-priority :: Char -> Int
-priority c
-    | c >= 'A' && c <= 'Z' = fromEnum c - fromEnum 'A' + 27
-    | otherwise            = fromEnum c - fromEnum 'a' + 1
+type Rucksack = [Int]
 
-partOne :: [String] -> Int
+aocParse :: Parser [Rucksack]
+aocParse = do
+    parseRuck `endBy` newline <* eof
+    where
+    parseRuck :: Parser Rucksack
+    parseRuck = do
+        map priority <$> some letterChar
+        where
+        priority :: Char -> Int
+        priority c
+            | c >= 'A' && c <= 'Z' = c' - intA + 27
+            | otherwise            = c' - inta + 1
+            where
+            c'   = fromEnum c
+            inta = fromEnum 'a' 
+            intA = fromEnum 'A'
+
+partOne :: [Rucksack] -> Int
 partOne = sum . map rucks
     where 
-    rucks :: String -> Int
+    rucks :: Rucksack -> Int
     rucks s = common (take h s) (drop h s)
         where
         h = (length s) `div` 2
-        common :: String -> String -> Int
+        common :: Rucksack -> Rucksack -> Int
         common [] _     = error "Unreachable"
         common (a : as) (bs)
-            | elem a bs = priority a
+            | elem a bs = a
             | otherwise = common as bs
 
-partTwo :: [String] -> Int
+partTwo :: [Rucksack] -> Int
 partTwo (a : b : c : ss) = sack3 a b c + partTwo ss
     where
-    sack3 :: String -> String -> String -> Int
+    sack3 :: Rucksack -> Rucksack -> Rucksack -> Int
     sack3 [] _ _               = error "Unreachable"
     sack3 (x : xs) y z
-        | elem x y && elem x z = priority x
+        | elem x y && elem x z = x
         | otherwise            = sack3 xs y z
 partTwo _                      = 0
