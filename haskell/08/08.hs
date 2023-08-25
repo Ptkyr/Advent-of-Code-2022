32ce28a7e7bc036dc722e034e8b294920b1b5edf
Ptkyr <ptkyr@ptkyr.localdomain>
2023-08-24 22:27:09 -0400

    day 8 parsing

--- haskell/08/08.hs
+++ haskell/08/08.hs
@@ -2,17 +2,12 @@ import Utils
 
 main :: IO ()
 main = do
-    input <- readFile "08/input.txt"
-    let forest = parseToArr $ words input
-    print $ partOne forest
-    print $ partTwo forest
-
-parseToArr :: [String] -> Arr2D Int
-parseToArr s = listArray ((1, 1), (x, y)) 
-             $ concat $ map (map digitToInt) s
-    where
-        x = length s
-        y = length $ head s
+    parsed <- parseInput aocParse "08/input.txt"
+    case parsed of
+        Left pError -> putStr $ errorBundlePretty pError
+        Right input -> do
+            print $ partOne input
+            print $ partTwo input
 
 partOne :: Arr2D Int -> Int
 partOne a = sum $ foldr (zipWithArr (ior)) u [l, d, r]
@@ -33,7 +28,7 @@ partTwo a = maximum $ foldr (zipWithArr (*)) u [l, d, r]
         r = viewDist id (+ 1)    a
 
 valueAt :: Int -> Int -> (Int -> Int -> Int)
-         -> (Int -> Int) -> (Int -> Int) -> Arr2D Int -> Arr2D Int
+        -> (Int -> Int) -> (Int -> Int) -> Arr2D Int -> Arr2D Int
 valueAt edge halt recur fx fy forest 
     = array bnds
     $ [((i, j), wave (forest!(i, j)) (fx i) (fy j))
@@ -50,7 +45,14 @@ valueAt edge halt recur fx fy forest
         where (nx, ny) = (fx u, fy v)
 
 visDir :: (Int -> Int) -> (Int -> Int) -> Arr2D Int -> Arr2D Int
-visDir   = valueAt 1 0 (*) 
+visDir = valueAt 1 0 (*) 
 
 viewDist :: (Int -> Int) -> (Int -> Int) -> Arr2D Int -> Arr2D Int
 viewDist = valueAt 0 1 (+) 
+
+aocParse :: Parser (Arr2D Int)
+aocParse = do
+    arr <- some digitChar `endBy` newline <* eof
+    let x = length arr
+    let y = length $ head arr
+    pure $ listArray ((1, 1), (x, y)) $ concat $ map (map digitToInt) arr
