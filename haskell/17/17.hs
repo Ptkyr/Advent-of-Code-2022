effbd91990ab837baa3c967dd0e8788a7dc53432
Ptkyr <ptkyr@ptkyr>
2023-09-07 11:40:31 -0400

    got it working again:

--- haskell/17/17.hs
+++ haskell/17/17.hs
@@ -4,7 +4,7 @@ import Debug.Trace
 
 main :: IO ()
 main = do
-    parsed <- parseInput aocParse "17/input.txt"
+    parsed <- parseInput aocParse "17/ex.txt"
     case parsed of
         Left pError -> putStr $ errorBundlePretty pError
         Right input -> do
@@ -13,8 +13,8 @@ main = do
 
 type Cave = Arr2D Bool
 
-partOne :: (Int -> Tetris) -> Int
-partOne tet = tetris $ tet 2022
+partOne :: Tetris -> Int
+partOne = tetris 2022
 
 showCave :: Cave -> String
 showCave cave = showHelp 1 $ elems cave
@@ -50,51 +50,61 @@ data Tetris = Tetris
     , _height :: Int
     , _clears :: Int
     , _place  :: Bool
-    , _total  :: Int
     }
 
 -- Call this with tetris (result of aocParse)
 --   which is the parsed input, rockOrder, an initial 0x0 cave
 --   matrix, 2022 for the count, 0 as initial height
-tetris :: Tetris -> Int
-tetris t@(Tetris (jet : js) (cur@(Rock shape rh ri) : rs) 
-          cave height clears place total)
-    | total == 0 = height
-    | place      = tetris t {_cave  = placeRock cur cave, 
-                             _place = False}
-    | canDrop    = tetris (Tetris js (downCur : rs) 
-                           cave height clears place total)
-    | otherwise  = tetris (Tetris js rs cave'
-                           height' clears True (total - 1))
+tetris :: Int -> Tetris -> Int
+tetris rocksLeft t@(Tetris 
+                    (jet : js) 
+                    (cur@(Rock shape rh ri) : rs) 
+                    cave height clears place)
+    | rocksLeft == 0 
+    = trace (show clears) axMax cave + clears + 1
+    | place      
+    = tetris rocksLeft t {_cave  = placeRock cur cave, 
+                          _place = False}
+    | canDrop    
+    = tetris rocksLeft (Tetris js (downCur : rs) 
+                        cave height clears place)
+    | otherwise  
+    = tetris (rocksLeft - 1) (Tetris js rs cave'
+                              height' clears' True)
     where
     transed = map jet shape
     jetted  = if validSpot transed cave
               then transed
               else shape
-    downed  = map pushDown jetted
-    pushDown (x, y) = (x + 1, y)
+    downed  = map (\(a, b) -> (a + 1, b)) jetted
     canDrop = validSpot downed cave
     downCur = Rock downed rh $ ri + 1
+
     newCave = cave // (zip jetted $ repeat True)
-    restRow = filledRow newCave [rh..rh + ri] -- implicit +1
+  
+    restRow = filledRow newCave [ri..ri + rh] -- implicit +1
+    clears' = clears + axMax newCave - restRow
     tncCave = listArray (origin, (restRow, 6)) $ elems newCave
-    height' = calcHeight height newCave
-    cave'   = listArray (origin, (height' - 1, 6)) 
-            $ drop (7 * emptyRows newCave)
-            $ elems newCave
-tetris _         = error "Shut up GHC you're unexhaustive"
+
+    height' = calcHeight height tncCave
+    empRows = emptyRows tncCave
+    cave'   = listArray (origin, (axMax tncCave - empRows, 6)) 
+            $ drop (7 * empRows)
+            $ elems tncCave
+tetris _ _       = error "Shut up GHC you're unexhaustive"
 
 filledRow :: Cave -> [Int] -> Int
 filledRow cave cands
-    | cands == []       = x -- return x to add 0 to cleared height
+    | cands == []       = axMax cave -- x adds 0 to clears'
+    | cand > x          = x
     | fullRow cave cand = cand
     | otherwise         = filledRow cave $ tail cands
-    where
-    bnds@(_, (x, _)) = bounds cave
+    where 
     cand = head cands
+    x = axMax cave
 
 calcHeight :: Int -> Cave -> Int
-calcHeight cur cave = x - highEmpty
+calcHeight cur cave = axMax cave - emptyRows cave
     where
     (_, (x, _)) = bounds cave
     row         = x - cur
@@ -147,7 +157,7 @@ type Jet = Coord -> Coord
 
 emptyarr :: Cave
 emptyarr = listArray ((1, 1), origin) []
-aocParse :: Parser (Int -> Tetris)
+aocParse :: Parser Tetris
 aocParse = do
     jets <- some parseJet <* newline <* eof
     pure $ Tetris (cycle jets)
